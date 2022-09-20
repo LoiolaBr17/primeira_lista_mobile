701121459684f15c6c4c680c70e9993a1a0e2cc8
LoiolaBr17 <mateustaua16@gmail.com>
2022-09-20 10:04:39 -0300

    tocando uma musica

---
+++ src/main/java/com/example/aula01/Adaptador.java
@@ -0,0 +1,2 @@
+package com.example.aula01;public class Adaptador {
+}
---
+++ src/main/java/com/example/aula01/playerMusical.java
@@ -0,0 +1,14 @@
+package com.example.aula01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class playerMusical extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player_musical);
+    }
+}
